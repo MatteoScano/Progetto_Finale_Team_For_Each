e27856fff6ac4926e037663963158d2e06378a2e
Luigi Pinna» <luigipinna3@gmail.com>
2021-10-21 20:37:34 +0200

    Aggiunto controllo doppio formato email sulla registrazione utente, controllo checkBox sui termini e condizioni implementato ma da ultimare

--- Angular_Frontend/Frontend/src/app/routes/registration/registration.component.ts
+++ Angular_Frontend/Frontend/src/app/routes/registration/registration.component.ts
@@ -12,10 +12,19 @@ import { Component, OnInit } from '@angular/core';
 export class RegistrationComponent implements OnInit {
 
   newUser : UserDataInterface;
+
   //variabili per il controllo password "uguale"
   password:string;
   confirmPassword:string;
-  invalidPassword=false;
+  passwordOk=true;
+
+   //controllo email valida
+   email:string;
+   emailOk=true;
+
+   //ccontrollo spunta termini e condizioni
+   terms:string;
+   termsOk=true;
 
   constructor(private registrationService : RegistrationService, private router:Router) { }
 
@@ -33,26 +42,65 @@ export class RegistrationComponent implements OnInit {
       return true
     }
   }
+  //verifica se l'email inserita è in un formato valido
+  checkEmail(form : NgForm):boolean{
+    this.email=form.form.value.email;
+    const at = "@";
+    const dotNet = ".";
+    if(this.email.includes(at) && this.email.includes(dotNet)){
+      console.log("Email valida! Contains: '@' and '.'");
+      return true;
+    }
+      else{
+        console.log("Email non valida");
+        return false;
+    } // true
+  }
+  //controllo spunta termini e condizioni
+  checkTerms(form : NgForm):boolean{
+    this.terms=form.form.value.terms;
+    console.log("terms dentro il check", this.terms)
+    if(this.terms){
+      this.termsOk=true;
+      return true;
+    }
+    else{
+      this.termsOk=false;
+      return false;
+    }
+  }
 
   //Crea un nuovo utente in base ai dati inseriti in input
   createUser(form : NgForm): void {
     let metched = this.checkPassword(form);
+    let emailChecked = this.checkEmail(form);
 
-    if(!metched){
-      this.invalidPassword=true;
-      return console.log("Password errata, Riprova");
-      }
-    else{
-      this.newUser = form.form.value;
-      this.newUser.enabled=1;
-      this.registrationService.addUser(this.newUser,"admin","admin").subscribe( results => {
-        console.log(results);
-        },
-        error=>{
-          console.log(error);
-        });
-        this.router.navigate(['/login']);
-  };
+   if(emailChecked){
+
+     if(metched){
+
+        this.newUser = form.form.value;
+        this.newUser.enabled=1;
+        this.registrationService.addUser(this.newUser,"admin","admin").subscribe( results => {
+          console.log("Password valida",results);
+          console.log("terms",this.terms);              //test
+          console.log("terms boolean ok",this.termsOk); //test
+          },
+          error=>{
+            console.log(error);
+          });
+          this.router.navigate(['/login']);
+     }
+     else{
+      this.emailOk=true;
+      this.passwordOk=false;
+      console.log("password errata, Riprova");
+     }
+   }
+   else{
+    this.emailOk=false;
+    console.log("Email Errata, Riprova!");
+   }
 }
 
 
